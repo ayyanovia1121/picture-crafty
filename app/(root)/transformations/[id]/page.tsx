11c22dde45b373ab0820611e710c09d468f59732
ayyanovia1121 <noviayya1121@gmail.com>
2024-11-28 13:13:18 +0700

    Route and Dynamic

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransformationPage = ({ params }: { params: { id: string } }) => {
+  return (
+    <div>TransformationPage</div>
+  )
+}
+
+export default TransformationPage
